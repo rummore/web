5fcbca51cf7525660d4cba95cb95be3110aba852
rummore <57300121+rummore@users.noreply.github.com>
2020-03-30 23:33:19 +0200

    Update index.js
    change on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,37 +15,20 @@ const IndexPage = ({data}) => (
     <section className="homeUs">
     <Container className="py-5">
       <Row>
+        <Col lg={3}></Col>
         <Col lg={6} className="py-5">
         <br></br>
         <h3 className="text-center">¿Cómo te puedo ayudar?</h3>
           <p class="text-center lead">Creo ideas, websites y videos para que muestren lo mejor de sus productos y / o servicios.</p>
         </Col>
-        <Col lg={6}>
+        {/* <Col lg={6}>
           <img src="http://www.rummore.com.ve/wp-content/themes/rummore/img/compus.gif" alt="Rummore" className="img-fluid"/>
-        </Col>
+        </Col> */}
+        <Col lg={3}></Col>
       </Row>
     </Container>
     </section>
-    <section className="homeClients py-5">
-    <Container className="py-5">
-    <h3 className="text-center">Clientes</h3>
-    {/* <h1>About {data.site.siteMetadata.title}</h1> */}
-   
-
-      <Row>
-      
-        <Col lg={4} className="text-center">
-        <img src="http://www.rummore.com.ve/wp-content/themes/rummore/img/logo1.png" alt="Rummore" className="img-fluid"/>
-        </Col>
-        <Col lg={4} className="text-center">
-          <img src="http://www.rummore.com.ve/wp-content/themes/rummore/img/logo2.png" alt="Rummore" className="img-fluid"/>
-        </Col>
-        <Col lg={4} className="text-center">
-          <img src="http://www.rummore.com.ve/wp-content/themes/rummore/img/logo3.png" alt="Rummore" className="img-fluid"/>
-        </Col>
-      </Row>
-    </Container>
-    </section>
+    
   </Layout>
 )
 
